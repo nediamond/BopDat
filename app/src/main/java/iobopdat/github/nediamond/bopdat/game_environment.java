85aba864d179a4bf7f10334d28cf0d7a80a002de
nediamond <nate.e.diamond@gmail.com>
2017-05-26 16:36:35 -0700

    displaying accel. output

--- app/src/main/java/iobopdat/github/nediamond/bopdat/game_environment.java
+++ app/src/main/java/iobopdat/github/nediamond/bopdat/game_environment.java
@@ -4,13 +4,59 @@ import android.app.Activity;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.content.Context;
 
-public class game_environment extends Activity {
+
+public class game_environment extends Activity implements SensorEventListener{
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    float[] gravity = new float[3];
+    float[] linear_acceleration = new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.activity_game_environment);
+
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorManager.registerListener(this, mSensor , SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event){
+        final float alpha = 0.8f;
+
+        // Isolate the force of gravity with the low-pass filter.
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+        // Remove the gravity contribution with the high-pass filter.
+        linear_acceleration[0] = event.values[0] - gravity[0];
+        linear_acceleration[1] = event.values[1] - gravity[1];
+        linear_acceleration[2] = event.values[2] - gravity[2];
+
+        ((TextView) findViewById(R.id.commandDisplay)).setText(Float.toString(linear_acceleration[0]));
+        ((TextView) findViewById(R.id.commandDisplay1)).setText(Float.toString(linear_acceleration[1]));
+        ((TextView) findViewById(R.id.commandDisplay2)).setText(Float.toString(linear_acceleration[2]));
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
     }
 }
