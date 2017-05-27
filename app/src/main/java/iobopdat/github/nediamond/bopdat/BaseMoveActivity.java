a14edf9f2566af25a6a1c04a86b7962308883839
bopdat <ucsbbopdat@gmail.com>
2017-05-27 10:33:00 -0700

    5/26 app update, work with accelerometers and the move activities

---
+++ app/src/main/java/iobopdat/github/nediamond/bopdat/BaseMoveActivity.java
@@ -0,0 +1,17 @@
+package iobopdat.github.nediamond.bopdat;
+
+import android.app.Activity;
+import android.hardware.SensorEventListener;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+import android.view.Window;
+import android.os.Bundle;
+
+/**
+ * Created by zach_meyer on 5/26/17.
+ */
+
+public abstract class BaseMoveActivity extends Activity {
+
+}
--- app/src/main/java/iobopdat/github/nediamond/bopdat/game_environment.java
+++ app/src/main/java/iobopdat/github/nediamond/bopdat/game_environment.java
@@ -2,61 +2,47 @@ package iobopdat.github.nediamond.bopdat;
 
 import android.app.Activity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.content.Context;
+import android.content.Intent;
 
 
-public class game_environment extends Activity implements SensorEventListener{
-    private SensorManager mSensorManager;
-    private Sensor mSensor;
-    float[] gravity = new float[3];
-    float[] linear_acceleration = new float[3];
+public class game_environment extends Activity {
+    final int MOVE_PICK = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.activity_game_environment);
-
-
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        mSensorManager.registerListener(this, mSensor , SensorManager.SENSOR_DELAY_NORMAL);
+        Intent intent = new Intent(game_environment.this,MoveActivity.class);
+        startActivityForResult(intent,MOVE_PICK);
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event){
-        final float alpha = 0.8f;
-
-        // Isolate the force of gravity with the low-pass filter.
-        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
-        // Remove the gravity contribution with the high-pass filter.
-        linear_acceleration[0] = event.values[0] - gravity[0];
-        linear_acceleration[1] = event.values[1] - gravity[1];
-        linear_acceleration[2] = event.values[2] - gravity[2];
-
-        ((TextView) findViewById(R.id.commandDisplay)).setText(Float.toString(linear_acceleration[0]));
-        ((TextView) findViewById(R.id.commandDisplay1)).setText(Float.toString(linear_acceleration[1]));
-        ((TextView) findViewById(R.id.commandDisplay2)).setText(Float.toString(linear_acceleration[2]));
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == MOVE_PICK && resultCode == RESULT_OK){
+            this.setContentView(R.layout.activity_game_environment);
+            ((TextView) findViewById(R.id.commandDisplay)).setText("Move Gud");
+        }
+        else{
+            this.setContentView(R.layout.activity_game_environment);
+            ((TextView) findViewById(R.id.commandDisplay)).setText("Move NotSoGud");
+        }
 
     }
+
 
     @Override
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this);
     }
 }
