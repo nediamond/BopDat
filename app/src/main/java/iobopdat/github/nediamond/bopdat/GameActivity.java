a11829fff0f42de3a5bffdca19d2287da029378c
bopdat <ucsbbopdat@gmail.com>
2017-06-02 16:28:10 -0700

    began move picking loop

--- app/src/main/java/iobopdat/github/nediamond/bopdat/GameActivity.java
+++ app/src/main/java/iobopdat/github/nediamond/bopdat/GameActivity.java
@@ -7,15 +7,24 @@ import android.view.Window;
 import android.widget.TextView;
 import android.content.Intent;
 
+import java.util.Random;
+
 
 public class GameActivity extends Activity {
     final int MOVE_PICK = 1;
+    final int LIFT_DAT = 0;
+    Random rand;
+    int choice;
+    int score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.setContentView(R.layout.activity_game_environment);
+
+        rand = new Random();
+        choice = rand.nextInt(4);
         Intent intent = new Intent(GameActivity.this, LiftDatActivity.class);
         startActivityForResult(intent,MOVE_PICK);
     }
@@ -23,8 +32,11 @@ public class GameActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == MOVE_PICK && resultCode == RESULT_OK){
-            this.setContentView(R.layout.activity_game_environment);
-            ((TextView) findViewById(R.id.commandDisplay)).setText("Move Gud");
+
+            if(choice == LIFT_DAT) {
+                Intent intent = new Intent(GameActivity.this, LiftDatActivity.class);
+                startActivityForResult(intent, MOVE_PICK);
+            }
         }
         else{
             this.setContentView(R.layout.activity_game_environment);
