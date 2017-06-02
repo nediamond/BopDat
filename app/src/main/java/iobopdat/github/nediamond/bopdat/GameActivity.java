fb389226a5d18e4c4dba4ef5713b8c3b199a0c72
nediamond <nate.e.diamond@gmail.com>
2017-06-02 16:41:21 -0700

    loop working

--- app/src/main/java/iobopdat/github/nediamond/bopdat/GameActivity.java
+++ app/src/main/java/iobopdat/github/nediamond/bopdat/GameActivity.java
@@ -12,9 +12,8 @@ import java.util.Random;
 
 public class GameActivity extends Activity {
     final int MOVE_PICK = 1;
-    final int LIFT_DAT = 0;
+    final int LIFT_DAT=0, PUSH_DAT=1, PULL_DAT=2, SHAKE_DAT=3;
     Random rand;
-    int choice;
     int score = 0;
 
     @Override
@@ -24,23 +23,18 @@ public class GameActivity extends Activity {
         this.setContentView(R.layout.activity_game_environment);
 
         rand = new Random();
-        choice = rand.nextInt(4);
-        Intent intent = new Intent(GameActivity.this, LiftDatActivity.class);
-        startActivityForResult(intent,MOVE_PICK);
+
+        startActivityForResult(chooseMove(),MOVE_PICK);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if(requestCode == MOVE_PICK && resultCode == RESULT_OK){
-
-            if(choice == LIFT_DAT) {
-                Intent intent = new Intent(GameActivity.this, LiftDatActivity.class);
-                startActivityForResult(intent, MOVE_PICK);
-            }
+            startActivityForResult(chooseMove(), MOVE_PICK);
         }
         else{
             this.setContentView(R.layout.activity_game_environment);
-            ((TextView) findViewById(R.id.commandDisplay)).setText("Move NotSoGud");
+            ((TextView) findViewById(R.id.commandDisplay)).setText("YOU A SUCKA");
         }
 
     }
@@ -49,5 +43,28 @@ public class GameActivity extends Activity {
     @Override
     protected void onPause() {
         super.onPause();
+    }
+
+    private Intent chooseMove() {
+        int choice = rand.nextInt(4);
+        Intent intent;
+        switch (choice) {
+            case LIFT_DAT:
+                intent = new Intent(GameActivity.this, LiftDatActivity.class);
+                break;
+            case PUSH_DAT:
+                intent = new Intent(GameActivity.this, PushDatActivity.class);
+                break;
+            case PULL_DAT:
+                intent = new Intent(GameActivity.this, PullDatActivity.class);
+                break;
+            case SHAKE_DAT:
+                intent = new Intent(GameActivity.this, ShakeDatActivity.class);
+                break;
+            default:
+                intent = new Intent(GameActivity.this, MainActivity.class);
+                break;
+        }
+        return intent;
     }
 }
